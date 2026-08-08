023fbde8cefd21e5009103c82a1104a0172298a1
NicolasD1440 <nicolascastiblanco6@gmail.com>
2026-08-07 20:27:02 -0500

    update3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
                 <Routes>
 
                     <Route
-                        path="/monitor"
+                        path="/"
                         element={<Home />}
                     />
 
