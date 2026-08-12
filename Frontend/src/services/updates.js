de1e61658acb7b841b4533aaa4099262d2d21d50
NicolasD1440 <nicolascastiblanco6@gmail.com>
2026-08-11 23:17:56 -0500

    .

--- Frontend/src/services/updates.js
+++ Frontend/src/services/updates.js
@@ -1,11 +1,11 @@
 import api from "./api";
 
-export const getUpdates = async () => {
+export const checkUpdates = async () => {
     const response = await api.get("/monitor/updates");
     return response.data;
 };
 
-export const applyUpdates = async () => {
+export const updateSystem = async () => {
     const response = await api.post("/monitor/update");
     return response.data;
 };
